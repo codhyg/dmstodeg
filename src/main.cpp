4a7cf102fba6288329e7e677004a22ca84081baf
Voidlinux User <codehygge@gmail.com>
2022-08-29 14:07:30 +0300

    upd minor improvement

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
     //report version
     std::cout << argv[0] << " Version " << dmstodeg_VERSION_MAJOR << '.' << dmstodeg_VERSION_MINOR << '.'
     << dmstodeg_VERSION_PATCH << '.' << BUILD_NUMBER << std::endl;
+    //end report version
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
