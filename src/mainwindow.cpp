0ec2913513f200ed881596c5b204373a4b9c255f
Voidlinux User <codehygge@gmail.com>
2022-08-31 16:22:41 +0300

    add basic user input to fields

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <iostream>
+#include <QIntValidator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,6 +11,11 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect(ui->calculateButton,SIGNAL(released()),this,SLOT(calculate_pressed()));
     connect(ui->reverseButton,SIGNAL(released()),this,SLOT(reverse_pressed()));
+    QValidator *int360Validator = new QIntValidator(0, 359, this);
+    QValidator *int60Validator = new QIntValidator(0, 59, this);
+    ui->dmsDegField->setValidator(int360Validator);
+    ui->dmsMinutesField->setValidator(int60Validator);
+    ui->dmsSecondsField->setValidator(int60Validator);
 }
 
 MainWindow::~MainWindow()
@@ -20,6 +26,14 @@ MainWindow::~MainWindow()
 void MainWindow::calculate_pressed()
 {
     std::cout << "calculate button pressed" << '\n';
+    QString degEditText = ui->degField->text();
+    std::cout << degEditText.toStdString().c_str() << '\n';
+    degEditText = ui->dmsDegField->text();
+    std::cout << degEditText.toStdString().c_str() << '\n';
+    degEditText = ui->dmsMinutesField->text();
+    std::cout << degEditText.toStdString().c_str() << '\n';
+    degEditText = ui->dmsSecondsField->text();
+    std::cout << degEditText.toStdString().c_str() << '\n';
 }
 
 void MainWindow::reverse_pressed()
