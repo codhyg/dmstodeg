93b3223d0e7be2f3815edd0c86bec42767ef216b
Voidlinux User <codehygge@gmail.com>
2022-12-19 14:19:31 +0300

    add if cause to cmake for crosscomp

--- include/config.h
+++ include/config.h
@@ -1,4 +1,4 @@
 // the congured options and settings for dmstodeg
 #define dmstodeg_VERSION_MAJOR 0
-#define dmstodeg_VERSION_MINOR 4
+#define dmstodeg_VERSION_MINOR 5
 #define dmstodeg_VERSION_PATCH 0
