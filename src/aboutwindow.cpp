13d4b07b7bb77ee3c6ff93a686d20906cb0b17ac
Voidlinux User <codehygge@gmail.com>
2023-01-27 11:33:29 +0300

    upd aboutlabel for auto version

--- src/aboutwindow.cpp
+++ src/aboutwindow.cpp
@@ -1,6 +1,8 @@
 #include "aboutwindow.h"
 #include "ui_aboutwindow.h"
 #include <QShortcut>
+#include "../build_number.hpp"
+#include "../include/config.h"
 
 AboutWindow::AboutWindow(QWidget *parent) :
     QDialog(parent),
@@ -10,6 +12,9 @@ AboutWindow::AboutWindow(QWidget *parent) :
 
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_W), this, SLOT(close()));
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), this, SLOT(close()));
+
+    QString dmstodeg_version = QString("<p><b>dmstodeg</b><br/>ver. %1.%2.%3<br/>build %4</p>").arg(dmstodeg_VERSION_MAJOR).arg(dmstodeg_VERSION_MINOR).arg(dmstodeg_VERSION_PATCH).arg(BUILD_NUMBER);
+    ui->aboutLabel->setText(dmstodeg_version);
 }
 
 AboutWindow::~AboutWindow()
