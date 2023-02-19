d7807e381808b7fec1333c4a21527d2325518f74
Ulysses <codehygge@gmail.com>
2023-02-19 22:20:45 +0300

    add windows app metadata to exe

---
+++ resources/img/appinfo.h
@@ -0,0 +1,22 @@
+#ifndef APPINFO_H
+#define APPINFO_H
+
+#define VER_FILEVERSION             0,7,0,0
+#define VER_FILEVERSION_STR         "0.7.0.0\0"
+
+#define VER_PRODUCTVERSION          0,7,0,0
+#define VER_PRODUCTVERSION_STR      "0.7\0"
+
+#define VER_COMPANYNAME_STR         "codhygge"
+#define VER_FILEDESCRIPTION_STR     "DMS TO DEG"
+#define VER_INTERNALNAME_STR        "dmstodeg"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2023 codhyg"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "dmstodeg.exe"
+#define VER_PRODUCTNAME_STR         "DMS to DEG"
+
+#define VER_COMPANYDOMAIN_STR       "github.com/codhyg/dmstodeg"
+
+#endif // APPINFO_H
+
